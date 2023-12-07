ee51b76aa262c2314c0768ad00e82e034e524444
KilianAlves <alveskilian@gmail.com>
2023-12-07 12:06:11 +0100

    fn de seance, erruer ajout cache

--- public/sw.js
+++ public/sw.js
@@ -2,18 +2,27 @@ const STATIC_CACHE_NAME = "todosApp-static.v0";
 
 const addResourcesToCache = async (ressources) => {
     const cache = await caches.open(STATIC_CACHE_NAME);
+    // erreur request fail ?
     return cache.addAll(ressources);
 }
-
+// Erreur dans les éléments mis en cache ?
 self.addEventListener("install", (event) => {
     event.waitUntil(
         addResourcesToCache([
             "/",
             "/index.html",
+            "/js/app.js",
             "/js/registerSw.js",
             "/css/style.css",
+            "/icons/icon-192x192.png",
+            "/icons/icon-512x512.png",
+            "/manifest.json",
         ])
     );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request));
 });
 
 
@@ -31,4 +40,5 @@ const cacheFirst = async (request) => {
     event.respondWith(cacheFirst(event.request));
   });
 
+// provoque erreur ??
 self.skipWaiting();
