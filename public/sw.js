a37d5a9fd7e9379b60d660517279bec66214ad55
KilianAlves <alveskilian@gmail.com>
2023-12-11 00:31:10 +0100

    avancement maison

--- public/sw.js
+++ public/sw.js
@@ -3,42 +3,40 @@ const STATIC_CACHE_NAME = "todosApp-static.v0";
 const addResourcesToCache = async (ressources) => {
     const cache = await caches.open(STATIC_CACHE_NAME);
     // erreur request fail ?
-    return cache.addAll(ressources);
+    await cache.addAll(ressources);
 }
+
+const cacheFirst = async (request) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+  const responseFromNetwork = await fetch(request);
+  addResourcesToCache(request, responseFromNetwork.clone());
+  return responseFromNetwork;
+};
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(cacheFirst({request: event.request}));
+});
+
 // Erreur dans les éléments mis en cache ?
 self.addEventListener("install", (event) => {
     event.waitUntil(
         addResourcesToCache([
             "/",
             "/index.html",
-            "/js/app.js",
+            "/assets/index-e1f2cdea.js",
             "/js/registerSw.js",
             "/css/style.css",
             "/icons/icon-192x192.png",
             "/icons/icon-512x512.png",
             "/manifest.json",
+            "/icons/favicon.ico"
         ])
     );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request));
-});
-
-
-const cacheFirst = async (request) => {
-    const responseFromCache = await caches.match(request);
-    if (responseFromCache) {
-      return responseFromCache;
-    }
-    const responseFromNetwork = await fetch(request);
-    addResourcesToCache(request, responseFromNetwork.clone());
-    return responseFromNetwork;
-  };
-  
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(cacheFirst(event.request));
-  });
 
 // provoque erreur ??
 self.skipWaiting();
