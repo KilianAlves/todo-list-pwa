474075b9276c0ce95c1aa57734ff4c54c4b52afa
KilianAlves <alveskilian@gmail.com>
2023-12-14 11:22:34 +0100

    fin de la mise en cache des données issues de l’api

--- public/sw.js
+++ public/sw.js
@@ -32,6 +32,47 @@ const cacheFirst = async (request) => {
   return responseFromNetwork;
 };
 
+// Mise en cache des données issues de l'API
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  // Vérifiez que la requête est une méthode GET et qu'elle concerne l'API (port :7000)
+  if (request.method === "GET" && request.url.includes(":7000/todos")) {
+    event.respondWith(
+      // Stratégie réseau en premier, puis cache en cas d'échec
+      fetch(request)
+        .then((networkResponse) => {
+          // Clonez la réponse pour la mettre en cache
+          const responseClone = networkResponse.clone();
+          caches.open("todos") // Créez ou ouvrez le cache 'todos'
+            .then((cache) => {
+              cache.put(request, responseClone); // Mettez en cache la réponse
+            });
+          return networkResponse; // Retournez la réponse réseau
+        })
+        .catch(() => {
+          // Si la requête échoue, cherchez dans le cache 'todos'
+          return caches.match(request)
+            .then((cachedResponse) => {
+              if (cachedResponse) {
+                return cachedResponse; // Retournez la réponse du cache
+              } else {
+                // Si la réponse n'est pas dans le cache, renvoyez une réponse vide ou un message d'erreur
+                return new Response("No data available in cache.", { status: 404 });
+              }
+            });
+        })
+    );
+  } else {
+    // Pour les autres requêtes, utilisez la stratégie de cacheFirst déjà définie
+    event.respondWith(cacheFirst(request));
+  }
+});
+
+
+// Si réseau indisponible, afficher un fond orange sinon le fond classique
+
 self.addEventListener("fetch", (event) => {
   // Si la requête cible une url contenant le fichier background.css
   if (event.request.url.includes("background.css")) {
